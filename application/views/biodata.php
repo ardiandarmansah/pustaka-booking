d05441b6e376bf7ad7759ec726b70af6e60db73e
ardiandarmansah <ardiandarmansah9@gmail.com>
2021-10-29 14:37:48 +0700

    first commit

---
+++ application/views/biodata.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>biodata saya</title>
+</head>
+<body>
+	<h2>"Perkenalkan Nama</h2>
+	<h2>Nama Saya Ardian Darmansah</h2>
+	<h2>Saya tinggal di daerah sukamulya</h2>
+	<h2>Olahraga saya yang saya sukai adalah volly" </h2>
+
+</body>
+</html>
